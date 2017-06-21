a9e7c249e869d14ece5a1963558378cc905e54ec
Martin <martintarres@gmail.com>
2017-06-21 12:15:42 -0300

    sacando avisos

--- src/main/java/ClaseObservador.java
+++ src/main/java/ClaseObservador.java
@@ -1,52 +1,37 @@
+import javax.swing.*;
 import java.util.Observable;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
-
-public class ClaseObservador extends Observable {
-	public int contentPaneSeleccionado;
-	public JPanel panel;
-	public JButton boton;
+class ClaseObservador extends Observable {
+	private int contentPaneSeleccionado;
+	private JPanel panel;
+	/*public JButton boton;*/
 	
-	public ClaseObservador(){
+	ClaseObservador(){
 		
 	}
-	
-	public void setPanel(int contentPaneSeleccionado){					// Este metodo sirve para setear
+
+	void setPanel(int contentPaneSeleccionado){					// Este metodo sirve para setear
 		this.contentPaneSeleccionado=contentPaneSeleccionado;			//	el panel que estamos viendo actualmente
 		setChanged();													// y notifica a los observers
 	    notifyObservers();
 	
 	}
 	
-	public int getPanelSeleccionado() {								// Se pasa un entero para saber a que
+	int getPanelSeleccionado() {								// Se pasa un entero para saber a que
 																	// panel nos estamos refiriendo
 		return contentPaneSeleccionado;
 	}	//
 
-	public void setVista(JPanel panel) {							// en los observers se carga el panel que vamos a ver
+	void setVista(JPanel panel) {							// en los observers se carga el panel que vamos a ver
 
 		this.panel=panel;
 
 	}
-	public JPanel getVista(){
+	JPanel getVista(){
 		//notifyObservers();
 		return panel;
 		
 	}
-	
-	public void setBoton(JButton boton){
-			this.boton=boton;
-	}
-	
-	public JButton getBoton(){
-		return boton;
-	}
-	
-	
-	
 
-	
-	
 
 }
