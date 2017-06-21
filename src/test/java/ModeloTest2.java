bb4decd99998e72a60acbbab497dafc5c765361b
Martin <martintarres@gmail.com>
2017-06-21 20:57:36 -0300

    subo modelotest2

---
+++ src/test/java/ModeloTest2.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by martin on 21/6/2017.
+ */
+public class ModeloTest2 extends Modelo {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void iniciarm() throws Exception {
+    }
+
+    @Test
+    public void cargar() throws Exception {
+    }
+
+    @Test
+    public void play() throws Exception {
+    }
+
+    @Test
+    public void pause() throws Exception {
+    }
+
+    @Test
+    public void stop() throws Exception {
+    }
+
+}
