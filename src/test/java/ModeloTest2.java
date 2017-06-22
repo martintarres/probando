653572d1a7f2a76c75cb91a9ba4363af4baf20d2
Martin <martintarres@gmail.com>
2017-06-21 21:07:40 -0300

    subiendo test

--- src/test/java/ModeloTest2.java
+++ src/test/java/ModeloTest2.java
@@ -1,5 +1,13 @@
+import javazoom.jlgui.basicplayer.BasicPlayer;
+import javazoom.jlgui.basicplayer.BasicPlayerException;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.awt.*;
+import java.io.File;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
 
 import static org.junit.Assert.*;
 
@@ -7,8 +15,34 @@ import static org.junit.Assert.*;
  * Created by martin on 21/6/2017.
  */
 public class ModeloTest2 extends Modelo {
+    private BasicPlayer player;
+    private File folder;
+    private List listatest;
+    private File folder1;
+    private String path;
+    private String path1;
+    private int incial;
+    private int finall;
+    private int resultadoalea;
+    private Random numeroalea;
+
+
     @Before
     public void setUp() throws Exception {
+        player= new BasicPlayer();
+        //path = "C:\\Users\\marti\\Music\\Coldplay_-_The_Scientist_RB-RcX5DS5A.mp3";
+        path = getClass().getResource("Coldplay_-_The_Scientist_RB-RcX5DS5A.mp3").getPath();
+        //path1 = "C:\\Users\\marti\\Music\\Luis_Fonsi_-_Despacito_ft_Daddy_Yankee_kJQP7kiw5Fk.mp3";
+        path1= getClass().getResource("Luis_Fonsi_-_Despacito_ft_Daddy_Yankee_kJQP7kiw5Fk.mp3").getPath();
+        folder =new File (path);
+        folder1= new File (path1);
+        try {
+            player.open(folder);
+        } catch (BasicPlayerException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        listatest = new List();
     }
 
     @Test
@@ -17,6 +51,11 @@ public class ModeloTest2 extends Modelo {
 
     @Test
     public void cargar() throws Exception {
+        assertEquals(0 , listatest.getItemCount());
+        listatest.add(folder.getName());
+
+
+        assertEquals(1 , listatest.getItemCount());
     }
 
     @Test
